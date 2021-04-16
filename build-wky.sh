a6ca9ff2e4496308ddfa2a3a53948ff53ed8e793
bugscaner <1006079161@qq.com>
2021-04-16 12:25:20 +0800

    编译玩客云 linux 5.10.30内核
    编译linux 5.10.30内核

--- build-wky.sh
+++ build-wky.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-BUILDB=$1
-
-if [ -z ${BUILDB} ]; then
-	echo "(buster | focal | bionic ....)??"
-	exit 1
-fi
 
 ./compile.sh  BOARD=aml-s812 BRANCH=dev RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=yes COMPRESS_OUTPUTIMAGE=sha,xz
